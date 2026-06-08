e954113cda33b3835413e493914fb5ef12bc4912
beheshta24 <beheshta.rahsepar@gmail.com>
2026-06-08 16:41:25 +0200

    Integrate ProseBuilder

--- src/main/java/de/htw_berlin/fb4/ossd/beheshta/App.java
+++ src/main/java/de/htw_berlin/fb4/ossd/beheshta/App.java
@@ -1,13 +1,15 @@
 package de.htw_berlin.fb4.ossd.beheshta;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import de.htw_berlin.fb4.ossd.prose.ProseBuilder;
+
+public class App {
+
+    public static void main(String[] args) {
+        ProseBuilder proseBuilder = new ProseBuilder();
+
+        proseBuilder.register(new GreetingSentence());
+        proseBuilder.register(new FarewellSentence());
+
+        System.out.println(proseBuilder.get());
     }
 }

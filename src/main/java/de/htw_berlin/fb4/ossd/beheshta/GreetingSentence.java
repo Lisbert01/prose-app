22305bb46d4e2e30fc7bd4a30f554c55196ca2c3
Lisbert01 <lisa.hausenblas@protonmail.com>
2026-07-04 13:38:01 +0200

    Add a Header with copyright and license information
    Added copyright notice and license information to GreetingSentence.java

--- src/main/java/de/htw_berlin/fb4/ossd/beheshta/GreetingSentence.java
+++ src/main/java/de/htw_berlin/fb4/ossd/beheshta/GreetingSentence.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2026 beheshta
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 package de.htw_berlin.fb4.ossd.beheshta;
 
 import de.htw_berlin.fb4.ossd.prose.Sentence;
